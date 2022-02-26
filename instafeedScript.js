c562417a2c1582156bd9c60058bf595505106604
Banapel <93161288+Banapel@users.noreply.github.com>
2022-02-26 14:02:29 -0800

    Add files via upload

---
+++ instafeedScript.js
@@ -0,0 +1,14 @@
+$(document).ready(function()  {
+                  var userFeed = new Instafeed({
+                  get:'user',
+                  userId:'',
+                  limit :12,
+                  resolution: 'standard_resolution',
+                  accessToken:'',
+                  sortBy: 'most-recent',
+                  template: 'section class="col-lg-3 gallery"><a herf="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></section>',
+                  });
+userFeed.run();
+  
+});
+                  
